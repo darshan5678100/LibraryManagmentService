381d9cd2af83e848bd89d7f1541d8f164a80fe82
darshan5678100 <64181790+darshan5678100@users.noreply.github.com>
2020-04-27 12:10:04 +0530

    Add files via upload

---
+++ LibraryException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentService.exception
+{
+    //here i am creating custom exception and inherited to Exception class
+   public class LibraryException : Exception
+    {
+        //here i am doing constructor chaining
+        public LibraryException() : base()
+            {
+            Console.WriteLine("OOppss something wents wrong!!");
+            }
+        public LibraryException(string Message) : base(Message)
+        {
+            Console.WriteLine("OOppss something wents wrong!!");
+        }
+        public LibraryException(string Message,Exception InnerException ) 
+        {
+
+        }
+       
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
